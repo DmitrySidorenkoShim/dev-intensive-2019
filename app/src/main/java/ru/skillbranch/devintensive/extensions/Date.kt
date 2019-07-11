8d815cc06bff8c1a1e098cd389458a0e2f98670e
sidorenko <sidorenko4er@gmail.com>
2019-07-11 14:52:19 +0300

    hometask_2 complete - fixed humanizeDiff()

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -29,15 +29,27 @@ fun Date.add(value:Int, units: TimeUnits = TimeUnits.SECOND) : Date{
 }
 
 fun Date.humanizeDiff(date: Date = Date()): String {
-    val diff = abs(this.time - date.time)
+    val diff = this.time - date.time
 
     return when(diff) {
+        in -999999999 * DAY .. -365 * DAY -> "более чем через год"
+        in -365 * DAY .. -48 * HOUR -> "${abs(diff/ DAY)} ${humDays(diff)} назад"
+        in -48 * HOUR .. -25 * HOUR -> "через 2 ${humDays(diff)}"
+        in -25 * HOUR .. -24 * HOUR -> "через день"
+        in -24 * HOUR .. -120 * MINUTE -> "через ${abs(diff/ HOUR)} ${humHours(diff)}"
+        in -120 * MINUTE .. -89 * MINUTE -> "через 2 ${humHours(diff)}"
+        in -89 * MINUTE .. -60 * MINUTE -> "через час"
+        in -60 * MINUTE .. -120 * SECOND -> "через ${abs(diff/ MINUTE)} ${humMinutes(diff)}"
+        in -120 * SECOND .. -89 * SECOND -> "через 2 ${humMinutes(diff)}"
+        in -89 * SECOND .. -60 * SECOND -> "через минуту"
+        in -60 * SECOND .. 0 * SECOND -> "через несколько секунд"
+
         in 0 * SECOND .. 60 * SECOND -> "несколько секунд назад"
-        in 60 * SECOND .. 60 * SECOND -> "минуту назад"
-        in 60 * SECOND .. 120 * SECOND -> "2 ${humMinutes(diff)} назад"
+        in 60 * SECOND .. 89 * SECOND -> "минуту назад"
+        in 89 * SECOND .. 120 * SECOND -> "2 ${humMinutes(diff)} назад"
         in 120 * SECOND .. 60 * MINUTE -> "${abs(diff/ MINUTE)} ${humMinutes(diff)} назад"
-        in 60 * MINUTE .. 60 * MINUTE -> "час назад"
-        in 61 * MINUTE .. 120 * MINUTE -> "2 ${humHours(diff)} назад"
+        in 60 * MINUTE .. 89 * MINUTE -> "час назад"
+        in 89 * MINUTE .. 120 * MINUTE -> "2 ${humHours(diff)} назад"
         in 120 * MINUTE .. 24 * HOUR -> "${abs(diff/ HOUR)} ${humHours(diff)} назад"
         in 24 * HOUR .. 25 * HOUR -> "день назад"
         in 25 * HOUR .. 48 * HOUR -> "2 ${humDays(diff)} назад"
@@ -49,7 +61,7 @@ fun Date.humanizeDiff(date: Date = Date()): String {
 }
 
 private fun humMinutes(diff: Long): String {
-    return when(diff) {
+    return when(abs(diff)) {
         1 * MINUTE -> "минуту"
         in 1 * MINUTE .. 4 * MINUTE -> "минуты"
         in 4 * MINUTE .. 59 * MINUTE -> "минут"
@@ -58,7 +70,7 @@ private fun humMinutes(diff: Long): String {
 }
 
 private fun humHours(diff: Long): String {
-    return when(diff) {
+    return when(abs(diff)) {
         1 * HOUR -> "час"
         in 1 * HOUR .. 4 * HOUR -> "часа"
         in 4 * HOUR .. 22 * HOUR -> "часов"
@@ -67,7 +79,7 @@ private fun humHours(diff: Long): String {
 }
 
 private fun humDays(diff: Long): String {
-    return when(diff) {
+    return when(abs(diff)) {
         1 * DAY -> "день"
         in 1 * DAY .. 4 * DAY -> "дня"
         in 4 * DAY .. 366 * DAY -> "дней"
