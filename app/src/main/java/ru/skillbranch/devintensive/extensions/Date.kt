c44f2c280c6ec72f723a7fe7125a6cd8bdde59ef
sidorenko <sidorenko4er@gmail.com>
2019-07-11 14:26:34 +0300

    hometask_2 complete - fixed humanizeDiff()

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -32,14 +32,16 @@ fun Date.humanizeDiff(date: Date = Date()): String {
     val diff = abs(this.time - date.time)
 
     return when(diff) {
-        in 0 * SECOND .. 59 * SECOND -> "несколько секунд назад"
-        in 59 * SECOND .. 61 * SECOND -> "минуту назад"
-        in 61 * SECOND .. 59 * MINUTE -> "${abs(diff/ MINUTE)} ${humMinutes(diff)} назад"
-        in 59 * MINUTE .. 61 * MINUTE -> "час назад"
+        in 0 * SECOND .. 60 * SECOND -> "несколько секунд назад"
+        in 60 * SECOND .. 60 * SECOND -> "минуту назад"
+        in 60 * SECOND .. 120 * SECOND -> "2 ${humMinutes(diff)} назад"
+        in 120 * SECOND .. 60 * MINUTE -> "${abs(diff/ MINUTE)} ${humMinutes(diff)} назад"
+        in 60 * MINUTE .. 60 * MINUTE -> "час назад"
         in 61 * MINUTE .. 120 * MINUTE -> "2 ${humHours(diff)} назад"
-        in 120 * MINUTE .. 23 * HOUR -> "${abs(diff/ HOUR)} ${humHours(diff)} назад"
-        in 23 * HOUR .. 25 * HOUR -> "день назад"
-        in 25 * HOUR .. 365 * DAY -> "${abs(diff/ DAY)} дней назад"
+        in 120 * MINUTE .. 24 * HOUR -> "${abs(diff/ HOUR)} ${humHours(diff)} назад"
+        in 24 * HOUR .. 25 * HOUR -> "день назад"
+        in 25 * HOUR .. 48 * HOUR -> "2 ${humDays(diff)} назад"
+        in 48 * HOUR .. 365 * DAY -> "${abs(diff/ DAY)} ${humDays(diff)} назад"
         in 366 * DAY .. 999999999 * DAY -> "более года назад"
         else -> "ошибка ${diff/ SECOND} секунд"
 
@@ -48,8 +50,8 @@ fun Date.humanizeDiff(date: Date = Date()): String {
 
 private fun humMinutes(diff: Long): String {
     return when(diff) {
-        in 1 * MINUTE .. 2 * MINUTE -> "минуту"
-        in 2 * MINUTE .. 5 * MINUTE -> "минуты"
+        1 * MINUTE -> "минуту"
+        in 1 * MINUTE .. 5 * MINUTE -> "минуты"
         in 5 * MINUTE .. 59 * MINUTE -> "минут"
         else -> "ошибка ${diff/ MINUTE} минут"
     }
@@ -57,10 +59,19 @@ private fun humMinutes(diff: Long): String {
 
 private fun humHours(diff: Long): String {
     return when(diff) {
-        in 1 * HOUR .. 2 * HOUR -> "час"
-        in 2 * HOUR .. 5 * HOUR -> "часа"
+        1 * HOUR -> "час"
+        in 1 * HOUR .. 5 * HOUR -> "часа"
         in 5 * HOUR .. 22 * HOUR -> "часов"
         else -> "ошибка ${diff/ HOUR} часов"
+    }
+}
+
+private fun humDays(diff: Long): String {
+    return when(diff) {
+        1 * DAY -> "день"
+        in 1 * DAY .. 5 * DAY -> "дня"
+        in 5 * DAY .. 366 * DAY -> "дней"
+        else -> "ошибка ${diff/ DAY} дней"
     }
 }
 
